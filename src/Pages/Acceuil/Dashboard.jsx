60c6167e3201fdfb859807541d40bc32ee45b548
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-05-18 09:59:26 -0700

    ads

--- src/Pages/Acceuil/Dashboard.jsx
+++ src/Pages/Acceuil/Dashboard.jsx
@@ -3,19 +3,37 @@ import './Dashboard.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Menucomponent from './component/Menucomponent'
-import { Box,Button } from '@mui/material'
 import TableauResultat from './component/TableauResultat'
 import MonCompt from './component/MonCompt'
 import Paramettre from './component/Paramettre'
+import Sorties from './component/Sorties'
 import { useNavigate } from 'react-router-dom'
-
+import { Stack,Box,Button,Link,TextField} from '@mui/material'
+import { useForm} from "react-hook-form"
+import  {toast} from 'react-hot-toast';
+import axios from 'axios';
 
 function Dashboard() {
-
+  const { register, handleSubmit,reset,formState:{errors} } = useForm();
   const navigate= useNavigate();
   const handleEntre=()=>{
      navigate("/entre")
   } 
+
+ const onSubmit=(data)=>{
+  // gateway pour les entre 
+  axios.post("http://localhost:3000/LesEntres",data).then((res)=>{
+    console.log(res)
+    toast.success("Depot reussie")
+    reset()
+    
+  }).catch((err)=>{
+    console.log(err)
+    toast.err("Il a une erreur")
+  })
+ }
+
+
   return (
     <>
     <div className='div_one'>
@@ -34,16 +52,7 @@ function Dashboard() {
       </Button>
     </Box></nav>
     <nav><Box>
-    <Button
-        sx={{color:"white"}}
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-     
-      >
-        Les sorties
-      </Button>
+    <Sorties />
     </Box></nav>
     <nav><Box>
     <Button
@@ -62,14 +71,48 @@ function Dashboard() {
     </div>
 
     <div className='div_two'>
-      <nav>
-        
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque eligendi quod non dicta assumenda. 
-          Laboriosam quam pariatur mollitia repellat nihil obcaecati, facilis fugiat quo neque et dolore, 
-          architecto omnis repudiandae.  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, odit accusantium beatae labore quasi suscipit quibusdam ea esse at totam excepturi in minus perferendis itaque consequuntur nobis adipisci natus. Facilis.</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque eligendi quod non dicta assumenda. 
-          Laboriosam quam pariatur mollitia repellat nihil obcaecati, facilis fugiat quo neque et dolore, 
-          architecto omnis repudiandae.  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem, odit accusantium beatae labore quasi suscipit quibusdam ea esse at totam excepturi in minus perferendis itaque consequuntur nobis adipisci natus. Facilis.</p>
+      <nav className='form_nav'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Dépôt d'argent</h1>
+        <br></br>
+        <Box sx={{
+            display:"grid",
+            gap:2
+        }}>
+  
+        <TextField id="filled-basic"  variant="outlined" type="date" fullWidth size='small' 
+         {...register("data_de_tansaction", { required:"Veillez entrez la date de transaction"})}/>
+          {errors.data_de_tansaction&& <span sx={{color:"red"}}>Ce champ est obligatoire</span>}
+
+        <TextField id="filled-basic" label="Provenence" variant="outlined" type="text" fullWidth size='small' 
+         {...register("Provenence", { required:"Veillez entrez la provenence",minLength:{required:
+            "Veillez entrez le provenence"
+         }})}/>
+          {errors.Provenence&& <span sx={{color:"red"}}>Ce champ est obligatoire</span>}
+
+          <TextField id="filled-basic" label="Montant" variant="outlined" type="number" fullWidth size='small' 
+         {...register("Montant", { required:"Veillez entrez le montant",minLength:{required:
+            "Veillez entrez le montant"
+         }})}/>
+          {errors.Montant&& <span sx={{color:"red"}}>Ce champ est obligatoire</span>}
+
+          <TextField id="filled-basic" label="Action" variant="outlined" type="text" fullWidth size='small' 
+         {...register("Action", { required:"Veillez entrez l'action ",minLength:{required:
+            "Veillez entrez l'action"
+         }})}/>
+
+  
+        </Box>
+        <Box sx={{
+          
+          display:"flex",
+          justifyContent: "space-between"
+        }}>
+        <Button variant="contained" sx={{marginTop:2}} type="submit" >Faire un dépôt</Button>
+        </Box>
+        </form>
+
+       
       </nav>
       <nav className='image_nav'>
         <img src="public\wallet_image-removebg-preview.png" alt="image" />
--- src/Pages/ComponentPages/LesEntres.jsx
+++ src/Pages/ComponentPages/LesEntres.jsx
@@ -1,23 +1,52 @@
 import React from 'react'
 import './components.css/LesEntres.css'
-
+import axios from 'axios'
+import { useState } from 'react';
 function LesEntres() {
+  const [datas,setdatas]=useState([])
+  
+  const getData = async () => {
+    
+    try {
+      // Fetch posts data
+      const data = await axios.get('http://localhost:3000/LesEntres');
+      setdatas(data.data[0]);
+  
+      
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  getData();
+    
   return (
     <div>
       <h1>page des entres</h1>
       <table>
         <thead>
-            <th><p>nom</p></th>
-            <th>post nom</th>
-            <th>pres nom</th>
-            <th>age</th>
+          <tr>
+          <th>Date de transaction</th>
+            <th>Provenence</th>
+            <th>Montant</th>
+            <th>Action</th>
+          </tr>
+            
         </thead>
-        <tbody>
-            <td>Eray</td>
-            <td>mutabesha</td>
-            <td>dieumerci</td>
-            <td>25</td>
+       
+{datas.map(dat=>(
+         <tbody>
+         <tr>
+         <td>{dat.data_de_tansaction}</td>
+         <td>{dat.Provenence}</td>
+         <td>{dat.Montant}</td>
+         <td>{dat.Action}</td>
+         </tr>
+         
         </tbody>
+        ))}
+       
+      
       </table>
     </div>
   )
