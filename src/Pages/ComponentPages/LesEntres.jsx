044491aabb617b484c862130d8ce8bebc94a489c
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-05-20 04:04:46 -0700

    entres end

--- src/Pages/ComponentPages/LesEntres.jsx
+++ src/Pages/ComponentPages/LesEntres.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import './components.css/LesEntres.css'
 import axios from 'axios'
 import { useState,useEffect } from 'react';
+import { Stack,Box,Button,Link,TextField} from '@mui/material'
+import { useNavigate } from 'react-router-dom';
+
+import TableauResultat from '../Acceuil/component/TableauResultat'
+import MonCompt from '../Acceuil/component/MonCompt'
+import Paramettre from '../Acceuil/component/Paramettre'
+import Sorties from '../Acceuil/component/Sorties'
+import Menucomponent from '../Acceuil/component/Menucomponent';
+import { Margin } from '@mui/icons-material';
+
 function LesEntres() {
   const [datas,setdatas]=useState([])
-  
+  const handleEntre=()=>{
+    navigate("/entre")
+ } 
+
 
 useEffect(()=>{
   const getData = async () => {
@@ -22,12 +35,50 @@ useEffect(()=>{
   
   getData();
 },[])
+let sommeDesEntres=0;
 
   
     
   return (
+    <>
+    <div className='div_one'>
+    <nav><Box><Menucomponent /></Box></nav>
+    <nav><Box><TableauResultat /></Box></nav>
+    <nav><Box>
+    <Button
+        sx={{color:"white"}}
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleEntre}
+      >
+        Les entres
+      </Button>
+    </Box></nav>
+    <nav><Box>
+    <Sorties />
+    </Box></nav>
+    <nav><Box>
+    <Button
+        sx={{color:"white"}}
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+     
+      >
+        Projets
+      </Button>
+    </Box></nav>
+    <nav><MonCompt/></nav>
+    <nav><Paramettre /></nav>
+    </div>
+
+
+
     <div>
-      <h1>page des entres</h1>
+      <h1>TABLEAU DES DEPOTS</h1>
       <table>
         <thead>
           <tr>
@@ -53,6 +104,7 @@ useEffect(()=>{
 </table>
   
     </div>
+    </>
   )
 }
 
